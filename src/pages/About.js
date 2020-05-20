e24651b3a3b21a0b3c3d43a4898b66c084a16bf2
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-05-20 17:12:28 +0900

    Change: About content

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,14 @@ const IndexPage = () => (
     <article id="main">
       <header>
         <h2>Who am I ?</h2>
-        <p>自己紹介ページへようこそ。</p>
+        <p>自己紹介ページへようこそ。ブログは下のボタンから。</p>
+        <a
+          href="https://techguy10308blog.netlify.app"
+          target="__blank"
+          className="button primary icon solid fa-newspaper"
+        >
+          My blog
+        </a>
       </header>
       <section className="wrapper style5">
         <div className="inner">
@@ -19,22 +26,12 @@ const IndexPage = () => (
               <img src={profilePic} alt="" />
             </span>
             広島県出身の駆け出しWebエンジニア。
-            13歳からずっと英語オタクで、洋画や洋楽をこよなく愛する。
-            大学3年次にロサンゼルスの日系コンサル企業にてインターンシップを経験し、ビジネスにも興味をもつ（主にマーケティング）。
-            大好きな英語を使いながら0から1を創り出せるプログラミングに魅力を感じ、2020年1月からTECH::CAMPにて学習開始。
-            スクールではサーバーサイドとしてRuby
-            onRailsを用いた個人・チーム開発、
-            独学では主にフロントサイドとしてReact/Gatsby.jsのようなモダンなJavaScriptライブラリを学習。
-          </p>
-          <p>
-            Vestibulum ultrices risus velit, sit amet blandit massa auctor sit
-            amet. Sed eu lectus sem. Phasellus in odio at ipsum porttitor mollis
-            id vel diam. Praesent sit amet posuere risus, eu faucibus lectus.
-            Vivamus ex ligula, tempus pulvinar ipsum in, auctor porta quam.
-            Proin nec dui cursus, posuere dui eget interdum. ブログは
-            <a href="https://techguy10308blog.netlify.app" target="__blank">
-              こちら
-            </a>
+            13歳からずっと英語オタクで、洋画や海外ドラマをこよなく愛する。
+            大学3年次にロサンゼルスの日系コンサル企業にてインターンシップを経験し、国際ビジネスやマーケティングに興味をもつ。
+            大好きな英語を使いながら0から1を創り出せるプログラミングに惹き込まれ、2020年1月からTECH::CAMPにて学習開始。
+            Ruby on
+            Rails中心のカリキュラム以外にも、ReactなどのモダンなJavaScriptライブラリやDockerを独学で習得して同年3月に卒業。
+            現在は、東京のITベンチャーでPythonを用いたバックエンド開発やネットワーク・インフラの構築を行っている。
           </p>
         </div>
       </section>
